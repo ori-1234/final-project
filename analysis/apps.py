60c16db2d4e37e53e09cbbe355af29b032a5a748
ori-1234 <ori.seibert12@gmail.com>
2025-03-09 17:29:39 +0200

    Updated sentiment analysis integration and database updates

--- analysis/apps.py
+++ analysis/apps.py
@@ -7,10 +7,10 @@ class AnalysisConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'analysis'
 
-    def ready(self):
-        """
-        This method is called when the application is ready.
-        We can use it to trigger the fetch_reddit_posts task.
-        """
-        # Call the fetch_reddit_posts task
-        fetch_reddit_posts.delay()  # Use .delay() to call it asynchronously
+    # def ready(self):
+    #     """
+    #     This method is called when the application is ready.
+    #     We can use it to trigger the fetch_reddit_posts task.
+    #     """
+    #     # Call the fetch_reddit_posts task
+    #     fetch_reddit_posts.delay()  # Use .delay() to call it asynchronously
--- backend/settings.py
+++ backend/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     # ... My apps ...
     'user',
     'analytics',
+    'analysis',
 ]
 
 MIDDLEWARE = [
